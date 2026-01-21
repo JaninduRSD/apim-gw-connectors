2cc94f01561ce95a8d2d7e5b52cea9feac9ab32f
janidu-wso2 <janidu@wso2.com>
2026-01-21 12:05:06 +0530

    resorce missing

--- azure/components/azure.gw.manager/src/main/java/org/wso2/azure/gw/client/AzureFederatedAPIDiscovery.java
+++ azure/components/azure.gw.manager/src/main/java/org/wso2/azure/gw/client/AzureFederatedAPIDiscovery.java
@@ -130,6 +130,11 @@ public class AzureFederatedAPIDiscovery implements FederatedAPIDiscovery {
                 }
                 else{
                     String apiDefinition = AzureAPIUtil.getRestApiDefinition(manager, httpClient, api);
+                    if (!AzureAPIUtil.hasResources(apiDefinition)) {
+                        log.warn("Skipping API '" + api.displayName() + "' (" + api.name() 
+                                + ") - API definition contains no resources/paths");
+                        continue;
+                    }
                     apiArtifact = AzureAPIUtil.restAPItoAPI(api, apiDefinition, organization, environment);
                 }
 
